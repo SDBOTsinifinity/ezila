8c821b5313a0369be9ee398b7f43828d3ee6ab85
SL Bots <84837445+slbotofficial11@users.noreply.github.com>
2021-06-25 23:23:48 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -49,7 +49,7 @@ Hello! my name *{}*.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/346d7934d46b03d4896c0.jpg"
+TECHNO_IMG = "https://telegra.ph/file/7bb512fd4c29eb943a13a.jpg"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
